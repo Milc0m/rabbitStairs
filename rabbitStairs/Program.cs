54b70bcd7e7bc567f26f726bbbc14b7c22569acd
unknown <dem82003@protonmail.com>
2019-04-20 12:21:25 +0300

    adding comments, adding function for input informaion controll

--- rabbitStairs/Program.cs
+++ rabbitStairs/Program.cs
@@ -22,22 +22,56 @@ namespace rabbitStairs
 
                 string[] words = textFromFile.Split(new[] { ' ', ',', ':', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
 
-                lineObj.k = Int16.Parse(words[0]);
-                lineObj.n = Int16.Parse(words[1]);
+                try
+                {
+                    lineObj.k = Int16.Parse(words[0]);
+                    lineObj.n = Int16.Parse(words[1]);
+                }
+                catch (Exception e) when (e is OverflowException || e is FormatException)
+                {
+                    Console.WriteLine("Enter a correct input information. Program will be close.");
+                    // Delay
+                    Console.ReadLine();
+                    Environment.Exit(-1);
+                }
+                
             }
 
+            // Checking user input and fix handling errors
+            if (lineObj.k < 1)
+            {
+                Console.WriteLine("Quantity of jumps can't be less than 1. Program will be close.");
+                // Delay
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
 
+            if (lineObj.n < lineObj.k)
+            {
+                Console.WriteLine("Quantity of stairs can't be less than quantity of jumps. Program will be close.");
+                // Delay
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+
+            if (lineObj.n > 300)
+            {
+                Console.WriteLine("The stair is too high for rabbit. It can't be grater than 300 steps. Program will be close.");
+                // Delay
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
 
 
             result = lineObj.Rabbit(lineObj.k, lineObj.n);
 
-            using (FileStream outFstream = new FileStream("output.txt", FileMode.OpenOrCreate))
+            using (FileStream outFstream = new FileStream("output.txt", FileMode.Create))
             {
                 string taskAnswer = result.ToString();
-
+                
                 byte[] input = Encoding.Default.GetBytes(taskAnswer);
                 outFstream.Write(input, 0, input.Length);
-                Console.WriteLine("Текст записан в файл");
+                Console.WriteLine("Answer was written in file.");
             }
 
             Console.ReadLine();
@@ -53,12 +87,17 @@ namespace rabbitStairs
 
         public int Rabbit(int k, int n)
         {
-            if (n < 0) return 0; // мы получили отрицательное число, значит в лестнице было меньше ступеней, чем была длина прыжка, этот способ добраться до вершины лестницы не работает
-            if (n == 0) return 1; // мы не вершине лестницы, предыдущий прыжок привел нас к цели
-            int s = 0; // считаем количество способов для текущей лестницы
-            for (int i = 1; i <= k; i++) // пробуем все варианты прыжков
+            // We get negative number, this means that our stairs is less than jump length. This method doesn't work
+            if (n < 0) return 0;
+            // We are at the top of the stairs, previous jump guide us to the target
+            if (n == 0) return 1;
+            // Calculating the quantity of possibilities for current stairs
+            int s = 0;
+            // Trying all possibilities of jumping
+            for (int i = 1; i <= k; i++) 
             {
-                s += Rabbit(k, n - i); // совершив прыжок на i ступеней, мы получим ту же задачу, но с лестницей, в которой n - i ступеней
+                // Making the jump for i step will guide us to the same task with a stair that have (n - i) steps
+                s += Rabbit(k, n - i);
             }
             return s;
         }
