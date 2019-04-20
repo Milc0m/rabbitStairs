64dd6a92ae9f85f7773046f06abc57585b1e09a2
unknown <dem82003@protonmail.com>
2019-04-20 10:54:58 +0300

    creating main logic

--- rabbitStairs/Program.cs
+++ rabbitStairs/Program.cs
@@ -9,116 +9,62 @@ namespace rabbitStairs
         public static void Main()
         {
 
+
+            int result;
+
             InputLine lineObj = new InputLine();
 
-            try
+ 
+            using (FileStream fstream = new FileStream("input.txt", FileMode.OpenOrCreate))
             {
-                using (FileStream fstream = new FileStream("input.txt", FileMode.Open))
-                {
-                    byte[] output = new byte[10];
-                    fstream.Read(output, 0, output.Length);
-                    var textFromFile = Encoding.Default.GetString(output);
+                byte[] output = new byte[10];
+                fstream.Read(output, 0, output.Length);
+                string textFromFile = Encoding.Default.GetString(output);
 
-                    string[] elements = textFromFile.Split(new[] { ','}, StringSplitOptions.RemoveEmptyEntries);
+                string[] words = textFromFile.Split(new[] { ' ', ',', ':', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
 
-                    try
-                    {
-                        if (elements.Length < 2)
-                        {
-                            Console.WriteLine("Needed at least 2 elements for correct work. Program will be close.");
-                            lineObj.Exit();
-                        }
-                        else
-                        {
-                            lineObj.Steps = int.Parse(elements[0]);
-                            lineObj.QtyStairs = int.Parse(elements[1]);
-                        }
-                        
-                    }
-                    catch (Exception e) when (e is OverflowException || e is FormatException)
-                    {
-                        Console.WriteLine("Enter a correct input information. Program will be close.");
-                        lineObj.Exit();
-                    }
-
-                }
+                lineObj.k = Int16.Parse(words[0]);
+                lineObj.n = Int16.Parse(words[1]);
             }
-            catch (FileNotFoundException)
+
+
+
+
+            result = lineObj.Rabbit(lineObj.k, lineObj.n);
+
+            using (FileStream outFstream = new FileStream("output.txt", FileMode.OpenOrCreate))
             {
-                Console.WriteLine("Could not find file \"input.txt\" in folder ...\\rabbitStairs\\bin\\Debug\\netcoreapp2.1. Program will be close. ");
-                lineObj.Exit();
-            }
-            
-            lineObj.CheckInput(lineObj.Steps, lineObj.QtyStairs);
-          
-            var result = lineObj.RabbitVariantCalculaiton(lineObj.Steps, lineObj.QtyStairs);
+                string taskAnswer = result.ToString();
 
-            var taskAnswer = result.ToString();
-
-
-            using (FileStream outFstream = new FileStream("output.txt", FileMode.Create))
-            {
                 byte[] input = Encoding.Default.GetBytes(taskAnswer);
                 outFstream.Write(input, 0, input.Length);
-                Console.WriteLine("Answer was written in file.");
+                Console.WriteLine("Текст записан в файл");
             }
 
             Console.ReadLine();
-
-            
         }
     }
 
     class InputLine
     {
-        public int Steps { get; set; }
-        public int QtyStairs { get; set; }
+        public string line { get; set; }
 
-        public int RabbitVariantCalculaiton(int k, int n)
+        public int k { get; set; }
+        public int n { get; set; }
 
+        public int Rabbit(int k, int n)
         {
-            // We get negative number, this means that our stairs is less than jump length. This method doesn't work
-            if (n < 0) return 0;
-            // We are at the top of the stairs, previous jump guide us to the target
-            if (n == 0) return 1;
-            // Calculating the quantity of possibilities for current stairs
-            int s = 0;
-            // Trying all possibilities of jumping
-            for (var i = 1; i <= k; i++) 
+            if (n < 0) return 0; // мы получили отрицательное число, значит в лестнице было меньше ступеней, чем была длина прыжка, этот способ добраться до вершины лестницы не работает
+            if (n == 0) return 1; // мы не вершине лестницы, предыдущий прыжок привел нас к цели
+            int s = 0; // считаем количество способов для текущей лестницы
+            for (int i = 1; i <= k; i++) // пробуем все варианты прыжков
             {
-                // Making the jump for i step will guide us to the same task with a stair that have (n - i) steps
-                s += RabbitVariantCalculaiton(k, n - i);
+                s += Rabbit(k, n - i); // совершив прыжок на i ступеней, мы получим ту же задачу, но с лестницей, в которой n - i ступеней
             }
             return s;
         }
-
-        public void CheckInput(int k, int n)
-        {
-            if (k < 1)
-            {
-                Console.WriteLine("Quantity of jumps can't be less than 1. Program will be close.");
-                Exit();
-            }
-            if (n < k)
-            {
-                Console.WriteLine("Quantity of stairs can't be less than quantity of jumps. Program will be close.");
-                Exit();
-            }
-            if (n > 300)
-            {
-                Console.WriteLine("The stair is too high for rabbit. It can't be grater than 300 steps. Program will be close.");
-                Exit();
-            }
-
-        }
-
-        public void Exit()
-        {
-            // Delay
-            Console.ReadLine();
-            Environment.Exit(-1);
-        }
     }
+
 
   
 
