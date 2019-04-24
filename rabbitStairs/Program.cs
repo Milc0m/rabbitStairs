4d428f6d7a6b329f34989be7b277f76528ac64e1
unknown <dem82003@protonmail.com>
2019-04-24 21:27:01 +0300

    implementing changes from review

--- rabbitStairs/Program.cs
+++ rabbitStairs/Program.cs
@@ -14,19 +14,18 @@ namespace rabbitStairs
 
             InputLine lineObj = new InputLine();
 
- 
-            using (FileStream fstream = new FileStream("input.txt", FileMode.OpenOrCreate))
+            using (FileStream fstream = new FileStream("input.txt", FileMode.Open))
             {
                 byte[] output = new byte[10];
                 fstream.Read(output, 0, output.Length);
-                string textFromFile = Encoding.Default.GetString(output);
+                var textFromFile = Encoding.Default.GetString(output);
 
-                string[] words = textFromFile.Split(new[] { ' ', ',', ':', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+                string[] elements = textFromFile.Split(new[] { ' ', ',', ':', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
 
                 try
                 {
-                    lineObj.k = Int16.Parse(words[0]);
-                    lineObj.n = Int16.Parse(words[1]);
+                    lineObj.Steps = int.Parse(elements[0]);
+                    lineObj.QtyStairs = int.Parse(elements[1]);
                 }
                 catch (Exception e) when (e is OverflowException || e is FormatException)
                 {
@@ -38,37 +37,13 @@ namespace rabbitStairs
                 
             }
 
-            // Checking user input and fix handling errors
-            if (lineObj.k < 1)
-            {
-                Console.WriteLine("Quantity of jumps can't be less than 1. Program will be close.");
-                // Delay
-                Console.ReadLine();
-                Environment.Exit(-1);
-            }
-
-            if (lineObj.n < lineObj.k)
-            {
-                Console.WriteLine("Quantity of stairs can't be less than quantity of jumps. Program will be close.");
-                // Delay
-                Console.ReadLine();
-                Environment.Exit(-1);
-            }
-
-            if (lineObj.n > 300)
-            {
-                Console.WriteLine("The stair is too high for rabbit. It can't be grater than 300 steps. Program will be close.");
-                // Delay
-                Console.ReadLine();
-                Environment.Exit(-1);
-            }
-
-
-            result = lineObj.Rabbit(lineObj.k, lineObj.n);
+            lineObj.CheckInput(lineObj.Steps, lineObj.QtyStairs);
+          
+            result = lineObj.RabbitVariantCalculaiton(lineObj.Steps, lineObj.QtyStairs);
 
             using (FileStream outFstream = new FileStream("output.txt", FileMode.Create))
             {
-                string taskAnswer = result.ToString();
+                var taskAnswer = result.ToString();
                 
                 byte[] input = Encoding.Default.GetBytes(taskAnswer);
                 outFstream.Write(input, 0, input.Length);
@@ -76,17 +51,17 @@ namespace rabbitStairs
             }
 
             Console.ReadLine();
+
+            
         }
     }
 
     class InputLine
     {
-        public string line { get; set; }
+        public int Steps { get; set; }
+        public int QtyStairs { get; set; }
 
-        public int k { get; set; }
-        public int n { get; set; }
-
-        public int Rabbit(int k, int n)
+        public int RabbitVariantCalculaiton(int k, int n)
         {
             // We get negative number, this means that our stairs is less than jump length. This method doesn't work
             if (n < 0) return 0;
@@ -95,12 +70,38 @@ namespace rabbitStairs
             // Calculating the quantity of possibilities for current stairs
             int s = 0;
             // Trying all possibilities of jumping
-            for (int i = 1; i <= k; i++) 
+            for (var i = 1; i <= k; i++) 
             {
                 // Making the jump for i step will guide us to the same task with a stair that have (n - i) steps
-                s += Rabbit(k, n - i);
+                s += RabbitVariantCalculaiton(k, n - i);
             }
             return s;
+        }
+
+        public void CheckInput(int k, int n)
+        {
+            if (k < 1)
+            {
+                Console.WriteLine("Quantity of jumps can't be less than 1. Program will be close.");
+                // Delay
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+            if (n < k)
+            {
+                Console.WriteLine("Quantity of stairs can't be less than quantity of jumps. Program will be close.");
+                // Delay
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+            if (n > 300)
+            {
+                Console.WriteLine("The stair is too high for rabbit. It can't be grater than 300 steps. Program will be close.");
+                // Delay
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+
         }
     }
 
